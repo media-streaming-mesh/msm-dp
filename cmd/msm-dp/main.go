1f0d4e419da3e9cae2e248e54812898affa673f3
Ammar Yasir Ali <ammar.y.alee@gmail.com>
2023-05-18 12:46:36 -0700

    testing the logging changes

--- cmd/msm-dp/main.go
+++ cmd/msm-dp/main.go
@@ -218,7 +218,7 @@ func main() {
 	go forwardRTPPackets(uint16(*rtpPort))
 	go forwardRTCPPackets(uint16(*rtpPort + 1))
 
-	util.Infof("Listening for messages coming from CP at ", lis.Addr())
+	util.Infof("Listening for messages coming from CP at %d", *port)
 
 	if err := s.Serve(lis); err != nil {
 		util.Fatalf("failed to serve: %v", err)
